ae5f4165fef4a6bf0c5f5f443d793ccaea2f54d8
vaarvik <47792591+vaarvik@users.noreply.github.com>
2022-11-09 22:08:37 +0100

    Refactor CustomOAuth2UserService

--- src/main/kotlin/no/authdemo/authdemo/security/oauth2/CustomOAuth2UserService.kt
+++ src/main/kotlin/no/authdemo/authdemo/security/oauth2/CustomOAuth2UserService.kt
@@ -4,10 +4,9 @@ import no.authdemo.authdemo.exception.OAuth2AuthenticationProcessingException
 import no.authdemo.authdemo.model.AuthProvider
 import no.authdemo.authdemo.model.User
 import no.authdemo.authdemo.repository.UserRepository
-import no.authdemo.authdemo.security.UserPrincipal.Companion.create
+import no.authdemo.authdemo.security.UserPrincipal
 import no.authdemo.authdemo.security.oauth2.user.OAuth2UserInfo
 import no.authdemo.authdemo.security.oauth2.user.OAuth2UserInfoFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.authentication.InternalAuthenticationServiceException
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
@@ -18,16 +17,16 @@ import org.springframework.stereotype.Service
 import org.springframework.util.StringUtils.hasLength
 
 @Service
-class CustomOAuth2UserService : DefaultOAuth2UserService() {
-    @Autowired
-    private lateinit var userRepository: UserRepository
+class CustomOAuth2UserService(val userRepository: UserRepository) : DefaultOAuth2UserService() {
 
+    // Think of this method as "onLoadOAuth2User"
     @Throws(OAuth2AuthenticationException::class)
     override fun loadUser(oAuth2UserRequest: OAuth2UserRequest): OAuth2User {
         val oAuth2User = super.loadUser(oAuth2UserRequest)
 
         return try {
-            processReceivedOAuth2User(oAuth2UserRequest, oAuth2User)
+            val oAuth2ProviderSlug = oAuth2UserRequest.clientRegistration.registrationId
+            handleReceivedOAuth2User(oAuth2ProviderSlug, oAuth2User)
         } catch (ex: AuthenticationException) {
             throw ex
         } catch (ex: Exception) {
@@ -36,31 +35,31 @@ class CustomOAuth2UserService : DefaultOAuth2UserService() {
         }
     }
 
-    private fun processReceivedOAuth2User(oAuth2UserRequest: OAuth2UserRequest, oAuth2User: OAuth2User): OAuth2User {
-        val oAuth2UserInfo = getOAuth2UserInfoFromProvider(oAuth2UserRequest, oAuth2User)
+    private fun handleReceivedOAuth2User(oAuth2ProviderSlug: String, oAuth2User: OAuth2User): OAuth2User {
+        val oAuth2UserInfo = getOAuth2UserInfoFromProvider(oAuth2ProviderSlug, oAuth2User)
 
-        if (!hasLength(oAuth2UserInfo.email)) {
+        if(!hasLength(oAuth2UserInfo.email))
             throw OAuth2AuthenticationProcessingException("Email not found from OAuth2 provider")
-        }
 
-        val optionalUser = userRepository.findByEmail(oAuth2UserInfo.email)
+        val potentialUser = userRepository.findByEmail(oAuth2UserInfo.email)
         var user: User
 
-        if (optionalUser.isPresent) {
-            user = optionalUser.get()
-            validateProviderUsedByExistingUser(oAuth2UserRequest, user)
+        if (potentialUser.isPresent) {
+            user = potentialUser.get()
+            validateProviderUsedByExistingUser(oAuth2ProviderSlug, user)
             user = updateExistingUser(user, oAuth2UserInfo)
         } else {
-            user = registerNewUser(oAuth2UserRequest, oAuth2UserInfo)
+            user = registerNewUser(oAuth2ProviderSlug, oAuth2UserInfo)
         }
-        return create(user, oAuth2User.attributes)
+
+        return UserPrincipal.create(user, oAuth2User.attributes)
     }
 
     private fun validateProviderUsedByExistingUser(
-        oAuth2UserRequest: OAuth2UserRequest,
+        oAuth2ProviderSlug: String,
         user: User
     ) {
-        val requestedProvider = AuthProvider.valueOf(oAuth2UserRequest.clientRegistration.registrationId)
+        val requestedProvider = AuthProvider.valueOf(oAuth2ProviderSlug)
         if (user.provider != requestedProvider) {
             throw OAuth2AuthenticationProcessingException(
                 "Looks like you're signed up with " +
@@ -70,21 +69,27 @@ class CustomOAuth2UserService : DefaultOAuth2UserService() {
         }
     }
 
-    private fun getOAuth2UserInfoFromProvider(oAuth2UserRequest: OAuth2UserRequest, oAuth2User: OAuth2User)
+    private fun getOAuth2UserInfoFromProvider(oAuth2ProviderSlug: String, oAuth2User: OAuth2User)
         = OAuth2UserInfoFactory.getOAuth2UserInfo(
-            oAuth2UserRequest.clientRegistration.registrationId,
+            oAuth2ProviderSlug,
             oAuth2User.attributes
     )
 
-    private fun registerNewUser(oAuth2UserRequest: OAuth2UserRequest, oAuth2UserInfo: OAuth2UserInfo): User {
+    private fun registerNewUser(oAuth2ProviderSlug: String, oAuth2UserInfo: OAuth2UserInfo): User {
+        val user = createOAuth2User(oAuth2ProviderSlug, oAuth2UserInfo)
+        return userRepository.save(user)
+    }
+
+    private fun createOAuth2User(oAuth2ProviderSlug: String,
+                                 oAuth2UserInfo: OAuth2UserInfo): User {
         val user = User()
-        val requestedProvider = AuthProvider.valueOf(oAuth2UserRequest.clientRegistration.registrationId)
+        val requestedProvider = AuthProvider.valueOf(oAuth2ProviderSlug)
         user.provider = requestedProvider
         user.providerId = oAuth2UserInfo.id
         user.name = oAuth2UserInfo.name
         user.email = oAuth2UserInfo.email
         user.imageUrl = oAuth2UserInfo.imageUrl
-        return userRepository.save(user)
+        return user
     }
 
     private fun updateExistingUser(existingUser: User, oAuth2UserInfo: OAuth2UserInfo): User {
--- src/main/kotlin/no/authdemo/authdemo/security/oauth2/user/OAuth2UserInfoFactory.kt
+++ src/main/kotlin/no/authdemo/authdemo/security/oauth2/user/OAuth2UserInfoFactory.kt
@@ -4,15 +4,15 @@ import no.authdemo.authdemo.exception.OAuth2AuthenticationProcessingException
 import no.authdemo.authdemo.model.AuthProvider
 
 object OAuth2UserInfoFactory {
-    fun getOAuth2UserInfo(registrationId: String, attributes: Map<String, Any>): OAuth2UserInfo {
-        return if (registrationId.equals(AuthProvider.google.toString(),ignoreCase = true)) {
+    fun getOAuth2UserInfo(oAuth2ProviderSlug: String, attributes: Map<String, Any>): OAuth2UserInfo {
+        return if (oAuth2ProviderSlug.equals(AuthProvider.google.toString(),ignoreCase = true)) {
             GoogleOAuth2UserInfo(attributes)
-        } else if (registrationId.equals(AuthProvider.facebook.toString(), ignoreCase = true)) {
+        } else if (oAuth2ProviderSlug.equals(AuthProvider.facebook.toString(), ignoreCase = true)) {
             FacebookOAuth2UserInfo(attributes)
-        } else if (registrationId.equals(AuthProvider.github.toString(), ignoreCase = true)) {
+        } else if (oAuth2ProviderSlug.equals(AuthProvider.github.toString(), ignoreCase = true)) {
             GithubOAuth2UserInfo(attributes)
         } else {
-            throw OAuth2AuthenticationProcessingException("Sorry! Login with $registrationId is not supported yet.")
+            throw OAuth2AuthenticationProcessingException("Login with $oAuth2ProviderSlug is not supported.")
         }
     }
 }
